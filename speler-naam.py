8c1651619fd501cba22d39ad24fe6fdf86e27a46
DraconicEternity <djblox.john@gmail.com>
2026-01-23 09:56:52 +0100

    [Update] Voorbeeld verwerkt in Highscore

--- speler-naam.py
+++ speler-naam.py
@@ -4,9 +4,3 @@ class SpelerNaam:
         self.naam = naam
     def get_naam(self):
         return self.naam
-
-# Voorbeeld van gebruik
-naamInput = input("Voer de naam van de speler in: ")
-spelerNaam = SpelerNaam(naamInput)
-
-print("De naam van de speler is:", spelerNaam.get_naam())
